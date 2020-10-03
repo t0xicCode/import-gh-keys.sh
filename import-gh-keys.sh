f1a3b81538682f95edc7475040546c14f58126b8
Xavier L <xavier.l@magnax.ca>
2020-10-03 15:33:16 -0400

    Fix issue #2

--- import-gh-keys.sh
+++ import-gh-keys.sh
@@ -6,6 +6,6 @@ read -p "Please type your github username: " USERNAME
 mkdir -p "$(dirname "${AUTH_FILE}")"
 chmod 700 "$(dirname "${AUTH_FILE}")"
 touch "${AUTH_FILE}"
-chmod 500 "${AUTH_FILE}"
+chmod 600 "${AUTH_FILE}"
 
 wget -O - "https://github.com/${USERNAME}.keys" >> "${AUTH_FILE}"
