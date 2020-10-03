367722a92787442d5667991b677d1885a578a21b
Xavier L <xavier.l@magnax.ca>
2020-10-03 15:18:33 -0400

    Create import-gh-keys.sh

---
+++ import-gh-keys.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+AUTH_FILE="${HOME}/.ssh/authorized_keys"
+read -p "Please type your github username: " USERNAME
+
+mkdir -p "$(dirname "${AUTH_FILE}")"
+chmod 700 "$(dirname "${AUTH_FILE}")"
+touch "${AUTH_FILE}"
+chmod 500 "${AUTH_FILE}"
+
+wget -O - "https://github.com/${USERNAME}.keys" >> "${AUTH_FILE}"
